597595e3ecc866d0063d160a1797276af3b5041b
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-11-03 16:29:12 -0700

    import browser router, link, switch route set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Link} from 'react-router-dom';
+// IMPORT LINK FROM REACT ROUTER DOM
+// SEE INDEX.JS FOR BROWSER ROUTER
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* PUT COMPONENT IN ROUTE W/IN SWITCH SO
+      ONLY SHOWS WHEN PATH USED, IF OUTSIDE THEN
+      COMPONENT IS ALWAYS SHOWN.*/}
+      <switch>
+        <Route path='/home'>
+          <Home/>
+        </Route>
+      </switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+// IMPORT BROWSERROUTER FROM REACT ROUTER DOM
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*WRAP TOP LEVEL <APP ELEMENT IN ROUTER*/}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
